d08c537f76fce7c0309d5b8fbde32699d7e0d62e
mhurtado13 <marcelo.hurt137@gmail.com>
2024-12-03 14:09:37 +0100

    First commit

--- starting_kit_phase1/attachement/Source_prior_known_features.R
+++
@@ -1,5 +0,0 @@
-# Source prior information. For example, a list of 100 genes and CpG probes (here randomly chosen).
-
-random_choosen_features = readRDS("attachement/random_choosen_features.rds")
-
-# we can also do some pretreatment on data here if wanted
--- starting_kit_phase1/attachement/additionnal_script.py
+++
@@ -1,4 +0,0 @@
-
-
-def useless_function():
-    print("This file only purpose is to show how to add modules or other scripts to your submissions.")
